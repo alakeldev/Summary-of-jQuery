30fc1cd82113dda323a54e000c7f20a38703bf91
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2023-11-15 13:33:50 +0100

    Added jQuery js files + starts adding notes on readme + inside the main.js file events sections

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # jQuery
+
+What is jQuery?
+it's a Library open source, it's build on Javascript.
+
+- With jQuery write less do more
+
+## Syntax
+$("p").hide();
+
+$ => jQuery Sign
+
+("") => HTML Element
+
+Hide() => Action/jQuery Method to do it on the html element
+
+
+- The different between js and jQuery
+
+JS:
+Here will start the function after the document loaded and load all the content inside including the images and videos..... of the website
+window.onload = function() {
+    // Write Your Code
+}
+
+
+jQuery:
+Here will start the function when the document ready and not after uploading all the content (images and ..... of the website)
+$(document).ready(function() {
+    // Write Your Code
+
+})
+
+
+- Short Code for the document ready
+
+$(function () {
+
+
+});
+
+
+#### Events:
---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+
+    // $("p").hide();
+    // $("p").css("color", "red");
+
+
+    // EVENTS 
+
+    //** click event
+    // $("button").click(function () {
+    //     // $("p").hide();
+
+    //     // $("p").css("color", "red");
+
+    //     // This word Important it's related to the button(same element)
+    //     // $(this).hide();
+    // });
+
+    /*-------------------------------------------------------------------------------*/
+
+    //** double click event
+    // $("button").dblclick(function () {
+    //     // $("p").hide();
+
+    //     // this word it's related to the button
+    //     // $(this).hide();
+    // });
+
+    /*-------------------------------------------------------------------------------*/
+
+    //** mouse enter
+    // $("button").mouseenter(function () {
+    //     $("p").css("color", "blue")
+    //     $(this).css("color", "#080");
+
+
+    // })
+
+
+    /*-------------------------------------------------------------------------------*/
+    //** mouse leave
+    // $("button").mouseleave(function () {
+    //     $("p").css("color", "red");
+    //     $(this).css("color", "#F00");
+    //     $("p").hide();
+
+    // });
+
+
+    /*-------------------------------------------------------------------------------*/
+
+
+    //** mouse hover => mouse enter + mouse leave  // the hover it takes two functions when mouse enter and when mouse leave
+    // $("button").hover(function () {
+    //     $(this).css("color", "#00F");
+    //     $("p").css("color", "#00F");
+    // },
+    //     function () {
+    //         $(this).css("color", "#080");
+    //         $("p").css("color", "#080");
+    //     });
+
+
+
+    /*-------------------------------------------------------------------------------*/
+
+    
+
+});
+
